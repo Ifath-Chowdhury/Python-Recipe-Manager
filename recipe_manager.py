22d80f6a95510b363554ad6138a9fa9b1e5f0817
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-06-04 00:29:47 +0100

    Made a command line interface for the program

--- recipe_manager.py
+++ recipe_manager.py
@@ -10,8 +10,37 @@ class RecipeList:
     def __init__(self):
         self.recipes = []
     
-    def AddRecipe(self, title, ingredients, instructions):
-        newRecipe = Recipe(title, ingredients, instructions)
+    def AddRecipe(self):
+        # Ask for new title
+        newTitle = input("Input the new title: ")
+
+        # Ask for new ingredients
+        newIngredients = []
+        numOfIngredients = input("How many ingredients to add? ")
+        if not numOfIngredients.isnumeric():
+            while not numOfIngredients.isnumeric():
+                numOfIngredients = input("We need a numeric input. How many ingredients to add? ")
+        else:
+            print(f"Adding {numOfIngredients} ingredients: ")
+            for i in range(1,numOfIngredients):
+                newItemtoAdd = input("Input ingredient to add: ")
+                newIngredients.append(newItemtoAdd)
+                i += 1
+
+        # Ask for new instructions
+        newInstructions = []
+        numOfInstructions = input("How many instructions in the recipe? ")
+        if not numOfInstructions.isnumeric():
+            while not numOfInstructions.isnumeric():
+                numOfInstructions = input("We need a numeric input. How many instructions in the recipe? ")
+        else:
+            print(f"Adding {numOfInstructions} instructions: ")
+            for i in range(1,numOfInstructions):
+                newItemtoAdd = input("Input instruction to add: ")
+                newInstructions.append(newItemtoAdd)
+                i += 1
+        
+        newRecipe = Recipe(newTitle, newIngredients, newInstructions)
         self.recipes.append(newRecipe)
     
     def ViewAllRecipes(self):
@@ -77,3 +106,23 @@ class RecipeList:
         matchingRecipe = self.SearchByTitle(titleToSearch)
         recipeToDelete = matchingRecipe[0]
         self.recipes.remove(recipeToDelete)
+    
+    def SaveToJSON(self):
+        pass
+
+    def LoadFromJSON(self):
+        pass
+
+def main():
+    #CLI interface
+    print("Welcome to the Recipe Manager!")
+    print("Here are your options, type the number corresponding to the option you want to pick:\n")
+    print("1. View all recipes")
+    print("2. Add a recipe")
+    print("3. Edit a recipe")
+    print("4. Search recipes by title")
+    print("5. Search recipes by ingredient")
+    print("6. Delete a recipe")
+
+if __name__ == "__main__":
+    main()
